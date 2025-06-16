ea76113de0c93f06eae60514846ec9402d66eb7a
kladenstien <siddharthraj.cs@gmail.com>
2025-06-16 21:45:18 +0530

    removed some more unused items

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,9 +1,3 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 
 import DashboardSidebar from "@/components/dashboard-sidebar";
 import DashboardRightbar from "@/components/dashboard-rightbar";
